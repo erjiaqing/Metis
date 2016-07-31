52bbc506916005b4fd59d4cf02f24422b0c593a4
B-L-J <nbcji@126.com>
2016-07-31 20:16:22 +0800

    Update 中国剩余定理.cpp

--- Templates/中国剩余定理.cpp
+++ Templates/中国剩余定理.cpp
@@ -1,14 +1,4 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
-using namespace std;
-
-const int N = 10005;
-int n;
-long long cir = 0, a[N], b[N];
-
-long long extended_Euclid(long long a, long long b, long long &x, long long &y) {
+long long extended_Euclid(long long a, long long b, long long &x, long long &y) { //return gcd(a, b) 
 	if (b == 0) {
 		x = 1;
 		y = 0;
@@ -23,7 +13,7 @@ long long extended_Euclid(long long a, long long b, long long &x, long long &y) 
 	}
 }
 
-long long China_Remainder(long long a[], long long b[], int n, long long &cir) { //a[]存放余数  b[]存放两两互质的除数
+long long China_Remainder(long long a[], long long b[], int n, long long &cir) { //a[]存放两两互质的除数  b[]存放余数
 	long long x, y, ans;
 	ans = 0; cir = 1;
 	for (int i = 1; i <= n; i++) cir *= a[i];
@@ -35,11 +25,25 @@ long long China_Remainder(long long a[], long long b[], int n, long long &cir) {
 	return (cir + ans % cir) % cir;
 }
 
-int main() {
-	while (scanf("%d", &n) == 1) {
-		for (int i = 1; i <= n; i++)
-			scanf("%d%d", &a[i], &b[i]);
-		printf("%lld\n", China_Remainder(a, b, n, cir));
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+bool merge(long long &a1, long long &b1, long long a2, long long b2) { //num = b1(mod a1), num = b2(mod a2)
+	long long x, y;
+	long long d = extended_Euclid(a1, a2, x, y);
+	long long c = b2 - b1;
+	if (c % d) return false;
+	long long p = a2 / d;
+	x = (c / d * x % p + p) % p;
+	b1 += a1 * x;
+	a1 *= a2 / d;
+	return true; 
+}
+
+long long China_Remainder2(long long a[], long long b[], int n) { //a[]存放除数(不一定两两互质)  b[]存放余数
+	long long x, y, ans, cir;
+	cir = a[1]; ans = b[1];
+	for (int i = 2; i <= n; i++) {
+		if (!merge(cir, ans, a[i], b[i])) return -1;
 	}
-	return 0;
+	return (cir + ans % cir) % cir;
 }
