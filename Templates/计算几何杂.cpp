bd52cda708844d2557bb91c7f8cf8045572d5403
xxxxxyt <inrence@outlook.com>
2016-10-08 15:31:20 +0800

    Create 计算几何杂.cpp

---
+++ Templates/计算几何杂.cpp
@@ -0,0 +1,29 @@
+bool pit_on_seg(pit a, pit b, pit c){ // 点在线段上
+	if(dcmp(det(b - a, c - a)) != 0) return false;
+	if(dcmp(dot(a - b, a - c)) > 0) return false;
+	return true;
+}
+bool pit_in_polygon(pit q){ // 点在多边形内
+	int cnt = 0;
+	for(int i = 1; i <= n; ++i){
+		pit p1 = p[i];
+		pit p2 = p[suc[i]];
+		if(pit_on_seg(q, p1, p2)) return true;
+		int k = dcmp(det(p2 - p1, q - p1));
+		int d1 = dcmp(p1.y - q.y);
+		int d2 = dcmp(p2.y - q.y);
+		if(k > 0 && d1 <= 0 && d2 > 0) ++cnt;
+		if(k < 0 && d2 <= 0 && d1 > 0) --cnt;
+	}
+	if(cnt != 0) return true;
+	else return false;
+}
+bool seg_in_polygon(pit a, pit b){ // 线段在多边形内 撒点
+	vec v = b - a;
+	for(int t = 1; t <= 1000; ++t){
+		pit c = a + v * (1.00 * (rand() % 10000) / 10000);
+		if(pit_in_polygon(c)) continue;
+		else return false;
+	}
+	return true;
+}
