91a9af4d3d7b2597db8aacda2e405b3d68cedf10
xxxxxyt <inrence@outlook.com>
2016-11-28 19:05:31 +0800

    Update ZKW费用流.cpp

--- Templates/ZKW费用流.cpp
+++ Templates/ZKW费用流.cpp
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 const int N = 4e3 + 5;
 const int M = 2e6 + 5;
 const long long INF = 1e18;
-
 struct eglist{
-	int sum;
-	int other[M], succ[M], last[N];
+	int tot_edge;
+	int dstn[M], nxt[M], lst[N];
 	long long cap[M], cost[M];
 	void clear(){
-		memset(last, -1, sizeof last);
-		sum = 0;
+		memset(lst, -1, sizeof lst);
+		tot_edge = 0;
 	}
 	void _addEdge(int a, int b, long long c, long long d){
-		other[sum] = b;
-		succ[sum] = last[a];
-		last[a] = sum;
-		cost[sum] = d;
-		cap[sum++] = c;
+		dstn[tot_edge] = b;
+		nxt[tot_edge] = lst[a];
+		lst[a] = tot_edge;
+		cost[tot_edge] = d;
+		cap[tot_edge++] = c;
 	}
 	void add_edge(int a, int b, long long c, long long d){
 		_addEdge(a, b, c, d);
 		_addEdge(b, a, 0, -d);
 	}
 }e;
-
-int st, ed;
-long long tot_flow, tot_cost;
-long long dist[N], slack[N];
-int vist[N], cur[N];
-
+int st, ed, vist[N], cur[N];
+long long tot_flow, tot_cost, dist[N], slack[N];
 int modlable(){
 	long long delta = INF;
 	for(int i = 1; i <= ed; ++i){
 		if(!vist[i] && slack[i] < delta)
 			delta = slack[i];
 		slack[i] = INF;
-		cur[i] = e.last[i];
+		cur[i] = e.lst[i];
 	}
 	if(delta == INF) return 1;
 	for(int i = 1; i <= ed; ++i)
@@ -45,7 +39,6 @@ int modlable(){
 			dist[i] += delta;
 	return 0;
 }
-
 long long dfs(int x, long long flow){
 	if(x == ed){
 		tot_flow += flow;
@@ -54,9 +47,9 @@ long long dfs(int x, long long flow){
 	}
 	vist[x] = 1;
 	long long left = flow;
-	for(int i = cur[x]; ~i; i = e.succ[i])
-		if(e.cap[i] > 0 && !vist[e.other[i]]){
-			int y = e.other[i];
+	for(int i = cur[x]; ~i; i = e.nxt[i])
+		if(e.cap[i] > 0 && !vist[e.dstn[i]]){
+			int y = e.dstn[i];
 			if(dist[y] + e.cost[i] == dist[x]){
 				long long delta = dfs(y, min(left, e.cap[i]));
 				e.cap[i] -= delta;
@@ -70,46 +63,13 @@ long long dfs(int x, long long flow){
 void minCost(){
 	tot_flow = 0, tot_cost = 0;
 	fill(dist + 1, dist + 1 + ed, 0);
-	for(int i = 1; i <= ed; ++i) cur[i] = e.last[i];
+	for(int i = 1; i <= ed; ++i) cur[i] = e.lst[i];
 	do{
 		do{
 			fill(vist + 1, vist + 1 + ed, 0);
 		}while(dfs(st, INF));
 	}while(!modlable());
 }
-
-int n, m, q, k;
-long long r, t;
-
 int main(){
-	
-	e.clear();
-	scanf("%d%d%I64d%I64d%d", &n, &m, &r, &t, &q);
-	k = min(1LL * m, t / r);
-	st = n + n + m + 1;
-	ed = n + n + m + 2;
-	for(int i = 1; i <= n; ++i) e.add_edge(st, i, m, 0);
-	for(int i = 1; i <= n; ++i)
-		for(int j = 1; j <= k; ++j)
-			e.add_edge(i, n + i, 1, r * j);
-	for(int i = 1; i <= m; ++i) e.add_edge(n + n + i, ed, 1, 0);
-	for(int qq = 1, i, j; qq <= q; ++qq){
-		scanf("%d%d", &i, &j);
-		e.add_edge(n + i, n + n + j, 1, 0);
-	}
-	minCost();
-	printf("%I64d %I64d\n", tot_flow, tot_cost);
-	for(int i = 1; i <= n; ++i){
-		long long tmp = 0;
-		int u = n + i;
-		for(int l = e.last[u]; ~l; l = e.succ[l]){
-			int j = e.other[l] - n - n;
-			if(j <= 0) continue;
-			if(e.cap[l]) continue;
-			printf("%d %d %I64d\n", i, j, tmp);
-			tmp += r;
-		}
-	}
-
-	return 0;
+	e.clear(); minCost();
 }
