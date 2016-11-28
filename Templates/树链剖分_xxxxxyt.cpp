91df3652a3419cd28210f49d188e9382ca3dfe7b
xxxxxyt <inrence@outlook.com>
2016-11-28 19:30:39 +0800

    Update 树链剖分_xxxxxyt.cpp

--- Templates/树链剖分_xxxxxyt.cpp
+++ Templates/树链剖分_xxxxxyt.cpp
@@ -3,12 +3,9 @@ struct qtree{
 	struct node{
 		int hson, top, size, dpth, papa, newid;
 	}nd[maxn];
-	
 	void find(int u, int fa, int d){
-		nd[u].hson = 0;
-		nd[u].size = 1;
-		nd[u].papa = fa;
-		nd[u].dpth = d;
+		nd[u].hson = 0; nd[u].size = 1;
+		nd[u].dpth = d; nd[u].papa = fa;
 		int max_size = 0;
 		for(int l = 0; l < mp[u].size(); ++l){
 			int v = mp[u][l].first;
@@ -23,8 +20,7 @@ struct qtree{
 		}
 	}
 	void connect(int u, int t){
-		nd[u].top = t;
-		nd[u].newid = ++tot;
+		nd[u].top = t; nd[u].newid = ++tot;
 		if(nd[u].hson != 0) connect(nd[u].hson, t);
 		for(int l = 0; l < mp[u].size(); ++l){
 			int v = mp[u][l].first;
@@ -36,7 +32,8 @@ struct qtree{
 		int rtn = -inf;
 		while(nd[u].top != nd[v].top){
 			if(nd[nd[u].top].dpth < nd[nd[v].top].dpth) swap(u, v);
-			rtn = max(rtn, st.query(1, 1, n, nd[nd[u].top].newid, nd[u].newid));
+			rtn = max(rtn, 
+				st.query(1, 1, n, nd[nd[u].top].newid, nd[u].newid));
 			u = nd[nd[u].top].papa;
 		}
 		if(nd[u].dpth > nd[v].dpth) swap(u, v);
@@ -54,7 +51,8 @@ struct qtree{
 	}
 	void clear(){
 		tot = 0;
-		nd[0].hson = nd[0].top = nd[0].size = nd[0].dpth = nd[0].papa = nd[0].newid = 0;
+		nd[0].hson = nd[0].top = nd[0].size = 0;
+		nd[0].dpth = nd[0].papa = nd[0].newid = 0;
 		for(int i = 1; i <= n; ++i) nd[i] = nd[0];
 	}
 }qt;
