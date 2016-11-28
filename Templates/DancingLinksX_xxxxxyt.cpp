fad01f0b24d98bfe218aff823431838fe4e8e116
xxxxxyt <inrence@outlook.com>
2016-11-28 18:54:54 +0800

    Update DancingLinksX_xxxxxyt.cpp

--- Templates/DancingLinksX_xxxxxyt.cpp
+++ Templates/DancingLinksX_xxxxxyt.cpp
@@ -3,8 +3,7 @@
 const int MAXN = 1e3 + 5;
 const int MAXM = MAXN * MAXN;
 const int INF = 1e9;
-int ans;
-int chosen[MAXM];
+int ans, chosen[MAXM];
 struct DancingLinks{
 	int row, col, tot;
 	int up[MAXM], dn[MAXM], lf[MAXM], rg[MAXM];
@@ -14,50 +13,36 @@ struct DancingLinks{
 	void init(int _n, int _m){
 		row = _n, col = _m;
 		for(int i = 0; i <= col; ++i){
-			sz[i] = 0;
-			up[i] = dn[i] = i;
-			lf[i] = i - 1;
-			rg[i] = i + 1;
+			sz[i] = 0; up[i] = dn[i] = i;
+			lf[i] = i - 1; rg[i] = i + 1;
 		}
-		rg[col] = 0;
-		lf[0] = col;
-		tot = col;
+		rg[col] = 0; lf[0] = col; tot = col;
 		for(int i = 1; i <= row; ++i) hd[i] = -1;
 	}
 	void lnk(int r, int c){
-		++tot;
-		++sz[c];
-		dn[tot] = dn[c];
-		up[tot] = c;
-		up[dn[c]] = tot;
-		dn[c] = tot;
-		posr[tot] = r;
-		posc[tot] = c;
+		++tot; ++sz[c];
+		dn[tot] = dn[c]; up[tot] = c;
+		up[dn[c]] = tot; dn[c] = tot;
+		posr[tot] = r; posc[tot] = c;
 		if(hd[r] < 0) hd[r] = lf[tot] = rg[tot] = tot;
 		else{
-			lf[tot] = hd[r];
-			rg[tot] = rg[hd[r]];
-			lf[rg[hd[r]]] = tot;
-			rg[hd[r]] = tot;
+			lf[tot] = hd[r]; rg[tot] = rg[hd[r]];
+			lf[rg[hd[r]]] = tot; rg[hd[r]] = tot;
 		}
 	}
 	void remove(int c){ // 删除列时删除能覆盖其的行
-		rg[lf[c]] = rg[c];
-		lf[rg[c]] = lf[c];
+		rg[lf[c]] = rg[c]; lf[rg[c]] = lf[c];
 		for(int i = dn[c]; i != c; i = dn[i])
 			for(int j = rg[i]; j != i; j = rg[j]){
-				dn[up[j]] = dn[j];
-				up[dn[j]] = up[j];
+				dn[up[j]] = dn[j]; up[dn[j]] = up[j];
 				--sz[posc[j]];
 			}
 	}
 	void resume(int c){
-		rg[lf[c]] = c;
-		lf[rg[c]] = c;
+		rg[lf[c]] = c; lf[rg[c]] = c;
 		for(int i = dn[c]; i != c; i = dn[i])
 			for(int j = rg[i]; j != i; j = rg[j]){
-				up[dn[j]] = j;
-				dn[up[j]] = j;
+				up[dn[j]] = j; dn[up[j]] = j;
 				++sz[posc[j]];
 			}
 	}
@@ -65,8 +50,7 @@ struct DancingLinks{
 		if(rg[0] == 0){
 			printf("%d", dpth);
 			for(int i = 0; i < dpth; ++i) printf(" %d", chosen[i]);
-			puts("");
-			return true;
+			puts(""); return true;
 		}
 		int c = rg[0];
 		for(int i = rg[0]; i; i = rg[i]) if(sz[i] < sz[c]) c = i;
@@ -85,16 +69,13 @@ DancingLinks dlx;
 int n, m;
 void work(){
 	dlx.init(n, m);
-	for(int i = 1; i <= n; ++i){
-		int k, j;
+	for(int i = 1, k, j; i <= n; ++i){
 		scanf("%d", &k);
-		while(k--){
-			scanf("%d", &j);
-			dlx.lnk(i, j);
-		}
+		while(k--) scanf("%d", &j), dlx.lnk(i, j);
 	}
 	if(!dlx.dance(0)) puts("NO");
 }
+
 // 重复覆盖
 // 给定一个 n 行 m 列的 0/1 矩阵，选择某些行使得每一列至少有一个 1
 struct DancingLinks{
@@ -104,49 +85,33 @@ struct DancingLinks{
 	void init(int _n, int _m){
 		row = _n, col = _m;
 		for(int i = 0; i <= col; ++i){
-			sz[i] = 0;
-			up[i] = dn[i] = i;
-			lf[i] = i - 1;
-			rg[i] = i + 1;
+			sz[i] = 0; up[i] = dn[i] = i;
+			lf[i] = i - 1; rg[i] = i + 1;
 		}
-		rg[col] = 0;
-		lf[0] = col;
-		tot = col;
+		rg[col] = 0; lf[0] = col; tot = col;
 		for(int i = 1; i <= row; ++i) head[i] = -1;
 	}
 	void lnk(int r, int c){
-		++tot;
-		++sz[c];
-		dn[tot] = dn[c];
-		up[dn[c]] = tot;
-		up[tot] = c;
-		dn[c] = tot;
+		++tot; ++sz[c];
+		dn[tot] = dn[c]; up[dn[c]] = tot;
+		up[tot] = c; dn[c] = tot;
 		if(head[r] < 0) head[r] = lf[tot] = rg[tot] = tot;
 		else{
-			rg[tot] = rg[head[r]];
-			lf[rg[head[r]]] = tot;
-			lf[tot] = head[r];
-			rg[head[r]] = tot;
+			rg[tot] = rg[head[r]]; lf[rg[head[r]]] = tot;
+			lf[tot] = head[r]; rg[head[r]] = tot;
 		}
 	}
 	void remove(int c){ // 删除列时不删除行 因为列可被重复覆盖
-		for(int i = dn[c]; i != c; i = dn[i]){
-			rg[lf[i]] = rg[i];
-			lf[rg[i]] = lf[i];
-		}
+		for(int i = dn[c]; i != c; i = dn[i])
+			rg[lf[i]] = rg[i], lf[rg[i]] = lf[i];
 	}
 	void resume(int c){
-		for(int i = up[c]; i != c; i = up[i]){
-			rg[lf[i]] = i;
-			lf[rg[i]] = i;
-		}
+		for(int i = up[c]; i != c; i = up[i])
+			rg[lf[i]] = i, lf[rg[i]] = i;
 	}
 	void dance(int d){
 		if(ans <= d) return;
-		if(rg[0] == 0){
-			ans = min(ans, d);
-			return;
-		}
+		if(rg[0] == 0){ans = min(ans, d); return;}
 		int c = rg[0];
 		for(int i = rg[0]; i != 0; i = rg[i]) if(sz[i] < sz[c]) c = i;
 		for(int i = dn[c]; i != c; i = dn[i]){ // 枚举c列是被哪行覆盖
